83024f36af5646db0731cc8fcd81dca55c84dfc6
Atharv Patil <83455141+atharv-patil@users.noreply.github.com>
2023-06-09 23:00:07 +0530

    Update 006 Best Time to Buy and Sell Stock.cpp

--- 006 Best Time to Buy and Sell Stock.cpp
+++ 006 Best Time to Buy and Sell Stock.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h> 
 int maximumProfit(vector<int> &prices){
     int n=prices.size();
-    vector<int>a(n);
-    for (int i=n-1;i>=0;i--)
-    {
-        if (i==n-1)
-            a[i] = prices[i];
-        else
-            a[i] = max(a[i+1],prices[i]);
-    }
+    int mn=prices[0];
     int ans = 0;
-    for (int i=0;i<n;i++)
-        ans = max(ans,-prices[i]+a[i]);
+    for (int i = 0; i < n; i++) {
+      ans = max(ans, prices[i] - mn);
+      mn= min(mn,prices[i]);
+    }
     return ans;
 }
