14fb81be1593bc92a5fee901de685b189d2f1532
Atharv Patil <83455141+atharv-patil@users.noreply.github.com>
2023-07-11 00:59:32 +0530

    Create 080 Maximum XOR of Two Numbers in an Array.cpp

---
+++ 080 Maximum XOR of Two Numbers in an Array.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+class TrieNode{
+public:
+    TrieNode *child[2];
+    
+    TrieNode(){
+        this->child[0] = NULL; 
+        this->child[1] = NULL;
+    }
+};
+
+    TrieNode *newNode;
+    
+    void insert(int x){   
+        TrieNode *t = newNode;
+        bitset<32> bs(x);
+        
+        for(int j=31; j>=0; j--){
+            if(!t->child[bs[j]]) t->child[bs[j]] = new TrieNode(); 
+            t = t->child[bs[j]];
+        }    
+    }
+    int maxXOR(int n){
+    TrieNode *t = newNode;
+    bitset<32> bs(n);
+    int ans=0; 
+    for(int j=31; j>=0; j--){
+        if(t->child[!bs[j]]) ans += (1<<j), t = t->child[!bs[j]]; 
+        
+        else t = t->child[bs[j]];
+    }
+    return ans;
+}
+
+    int maximumXor(vector<int>& nums) {
+        newNode = new TrieNode();
+        for(auto &n : nums) insert(n);
+        
+        int ans=0; 
+        for(auto n : nums){
+            ans = max(ans, maxXOR(n));  
+        }
+        return ans;
+    }
+    
+
+
